1edacbd63cb9bc88464e2551a0d7966c580cd425
0riginRobot <94578836+0riginRobot@users.noreply.github.com>
2021-12-22 19:44:45 -0700

    Add files via upload
    copy and paste this into a .java file. DO NOT TRY TO DOWNLOAD. It will mess up your other code. just create a filed named Sweeper.java and copy/paste the code into the .java file. Again, DO NOT DOWNLOAD!!!!

---
+++ Sweeper.java
@@ -0,0 +1,55 @@
+package frc.robot;
+
+//import your motor controllers, compressors, solenoids, and physical controllers using this pattern.
+    import edu.wpi.first.wpilibj.XboxController;
+    import edu.wpi.first.wpilibj.Spark;
+    import edu.wpi.first.wpilibj.Compressor;
+    import edu.wpi.first.wpilibj.Solenoid;
+
+public class Sweeper {
+//set your pistons to default positions
+    public static boolean fourBarS = false;
+    public static boolean compressorT = false;
+//name your motors, controller, and make the public static void drive here.
+    public static Spark left = new Spark(0);
+    public static Spark right = new Spark(1);
+    public static Spark grab = new Spark(2);
+    public static XboxController driver = new XboxController(3);
+    public static Compressor compressor = new Compressor();
+    public static Solenoid fourBar - new Solenoid(blank);
+    public static void drive(){
+//set your speeds
+    double speed = 0.5;
+    double grabber = 0.2;
+    //drivetrain
+        left.set(driver.getRawAxis(1) *speed);
+        right.set(driver.getRawAxis(3)*speed);
+        //graber on Sweeper
+        if (driver.getRawButton(8)){
+            grab.set(grabber);
+        } else if (driver.getRawButton(7)); {
+            grab.set(-grabber);
+        } else {
+            grab.set(0);
+        }
+        //piston controls
+        if (driver.getRawButtonPressed(2)){
+            if (compressorT) {
+                compressor.start();
+                compressorT = false;
+            } else if (!compressorT) {
+                compressor.stop();
+                compressorT = true;
+            }
+            if (driver.getRawButtonPressed(3)) {
+                if (fourBarS) {
+                    fourBar.startPulse();
+                    fourBarS = false;
+                } else if (!fourBarS) {
+                    fourBar.startPulse();
+                    fourBarS = true;
+                }
+            }
+        }
+    }
+}
